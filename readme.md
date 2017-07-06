8683291d22136ba97072a4cdae74937eba2263e4
Andrew Whitley <awhitley1233@gmail.com>
2017-07-05 21:25:29 -0400

    add first section

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # JavaScript Checkpoint
+
+
+## Part 1
+- Follow the prompts and write your code in `script.js`
---
+++ script.js
@@ -0,0 +1,16 @@
+// Create an array called `colors` that contains three colors.
+
+
+// Access the last item in the array and assign to a variable called `last`.
+
+
+// Create an empty array called `favoriteColors`.
+
+
+// Create a `for` loop that pushes each item in `colors` into `favoriteColors`.
+
+
+// Create an `instructor` object that contains three key-value pairs.
+
+
+// Add a `facial-hair` property to `instructor` by accessing it (do not change the original object you typed above).
