470910df34a523eed06be363db79c41fa3fd9b4e
Emmanuel Asu <mannyasu2@gmail.com>
2019-02-19 14:32:05 -0500

    test

--- hof.js
+++ hof.js
@@ -28,7 +28,11 @@ var ghostBusters = [
 // called `characterNames`.
 // Type your solution immediately below this line:
 
+var characterNames = ghostBusters.map(function())
+
 // #2: Use the `filter` array method to create a new, filtered array containing
 // only characters from the `ghostBusters` array who have 10 or more ghost KOs.
 // Assign the returned array to a variable called `superBusters`.
 // Type your solution immediately below this line:
+let newarray = superBusters
+ghostbusters.filter()g
--- oojs.js
+++ oojs.js
@@ -2,14 +2,26 @@
 
 // #1: Define a `Student` class with the following properties:
 // - a `name` property for their first and last name
-// - a `class` property for the class they're a student in
+// - a `course` property for the class they're a student in
 // - a `grade` property for their grade in the class
 // - a `graduate` method that sets the `grade` property to the string `A+`
 // Type your solution immediately below this line:
 
+class Student {
+  constructor(name, course, grade) {
+    (this.name = name), (this.course = course), (this.grade = grade);
+  }
+  graduate() {
+    console.log("get an A+");
+  }
+}
+
 // #2: Create an instance of the Student class and set it to a variable called `myStudent`
 // Call the instance's `graduate` method to set the student's grade to an 'A+'
 // Type your solution immediately below this line:
+
+var myStudent = new Student(name, course, grade);
+graduate();
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
 if (typeof Student !== "undefined") {
