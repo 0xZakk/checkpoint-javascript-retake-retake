90c5a27d8edc62245fae9a5d30b428b3de25d953
Leonard Busenah <busenah@gmail.com>
2019-02-19 14:28:02 -0500

    addition to classes

--- oojs.js
+++ oojs.js
@@ -6,20 +6,25 @@
 // - a `grade` property for their grade in the class
 // - a `graduate` method that sets the `grade` property to the string `A+`
 // Type your solution immediately below this line:
-class Student {
-  constructor(firstName, lastName);
-    course: "WDI"
-    grade: "cohort 28"
-    graduate: 2018
 
+class Student {
+  constructor(firstName, lastName) {
+    this.name = "name";
+    this.course = "course";
+    this.grade = "grade";
+    this.graduate = "graduate"
+  }
 }
 
 // #2: Create an instance of the Student class and set it to a variable called `myStudent`
 // Call the instance's `graduate` method to set the student's grade to an 'A+'
 // Type your solution immediately below this line:
+
 class myStudent {
-  constructor(firstName, lastName);
-    super myStudent extends Student
+  constructor(firstName, lastName) {
+    super extends class Student
+
+}
 }
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
