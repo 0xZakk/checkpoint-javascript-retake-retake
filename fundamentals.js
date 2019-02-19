fa487ae4221af89deed303c48a2b62fd5815d9de
Floydlholt <32264287+Floydlholt@users.noreply.github.com>
2019-02-19 13:52:27 -0500

    JS retake
    added JS

--- fundamentals.js
+++ fundamentals.js
@@ -8,14 +8,29 @@ var superHeroes = ["batman", "superman", "spiderman"]
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
+console.log(superHeroes);
+
 // #3: Create an empty array called `favoriteHeroes`.
 // Type your solution immediately below this line:
+
+var favoriteHeroes = []
 
 // #4: Create a `for` loop that adds each string in `superHeroes` to `favoriteHeroes`.
 // Type your solution immediately below this line:
 
+for (let superHeroes = []; superHeroes < favoriteHeroes.length; superHeroes++) {
+    const element = array superHeroes;
+    
+}
+
 // #5: Create an object literal called `batman` that contains three key-value pairs.
 // Type your solution immediately below this line:
+
+var batman = {
+animal = "bat",
+age = 40,
+son = "boy", 
+}
 
 // #6: Add a `has-secret-lair` (spelled exactly) property to `batman` by accessing
 // it (do not change the original object you typed above) and assigning it
